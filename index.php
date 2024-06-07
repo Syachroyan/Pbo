0ae798d06b4106b7add04de49466d46912d8b584
Syachroyan <syahroyan7@gmail.com>
2024-06-07 11:56:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b> Toko Royan </b></a>
+    <a href="../../index2.html"><b> Aplikasi Toko Pertanian </b></a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
